85e1fe3d5eaaa31c08f7fcf39cd6a4132b7fba50
Meeba-The-Programmer <47075718+Meeba-The-Programmer@users.noreply.github.com>
2025-07-30 18:37:21 +0000

    WIP Spider

--- myproject/myproject/spiders/charity_profiles_spider.py
+++ myproject/myproject/spiders/charity_profiles_spider.py
@@ -3,14 +3,25 @@
 '''
 
 import scrapy
-from playwright.async_api import async_playwright
+from scrapy_playwright.page import PageMethod
 
-class CharityProfileSpider(scrapy.Sider):
+class CharityProfileSpider(scrapy.Spider):
     name = "organisation_profile" # unique identifier for the Spider
-    start_urls = ["data:,"]
+    start_urls = ["https://www.charities.gov.sg/Pages/AdvanceSearch.aspx"]
 
-    async def parse(self):
-        urls = ["https://www.charities.gov.sg/Pages/AdvanceSearch.aspx"]
+    # function to have playwright click on search button to load charity profiles
+    def start_requests(self):
+        yield scrapy.Request(
+            url=self.start_urls[0],
+            meta={
+                "playwright": True,
+                "playwright_page_methods": [
+                    PageMethod("click", "button#btnSearchClk"), # referenced search button by ID
+                ],
+            },
+            callback=self.parse,
+            )
 
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    # function to process page content after button click
+    def parse(self, response):
+        pass
