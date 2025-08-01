57b8485b82f6befeacef9f0274ebe15554049653
Meeba-The-Programmer <47075718+Meeba-The-Programmer@users.noreply.github.com>
2025-08-01 03:54:59 +0000

    WIP org profile spider

--- myproject/myproject/spiders/charity_profiles_spider.py
+++ myproject/myproject/spiders/charity_profiles_spider.py
@@ -30,8 +30,22 @@ class CharityProfileSpider(scrapy.Spider):
         # Test: extract current page charity links first
         # Extract charity links
         charity_links = response.css('a[rel="noopener noreferrer"]::attr(href)').getall()
-        for charity in charity_links:
-            yield response.follow(charity, self.parse_charity_profiles) 
+
+        for link in charity_links:
+            yield scrapy.Request(
+                url=response.urljoin(link),
+                meta={
+                    "playwright": True,
+                    "playwright_page_methods": [
+                        # waiting for org profile tab to be active
+                        PageMethod("wait_for_selector", 'li[data-target="#tabProfile"][data-state="active"]', timeout=15000),
+                        # waiting for tab content to load 
+                        PageMethod("wait_for_selector", 'div#tabProfile', timeout=15000),
+                        PageMethod("wait_for_load_state", "networkidle"),
+                    ],
+                },
+                callback=self.parse_charity_profile
+            )
             
     # function to extract relevant org profile data from charity page
     # issue: need Playwright to help wait for the page to load the data    
@@ -49,9 +63,33 @@ class CharityProfileSpider(scrapy.Spider):
             "sector_administrator": response.css('//*[@id="tabProfile"]/div[6]/div[2]/p::text').get(),
             "website": response.css('//*[@id="tabProfile"]/div[7]/div[1]/p/a::attr(href)').get(),
             "last_profile_update": response.css('//*[@id="tabProfile"]/div[7]/div[2]/p::text').get(), # very important field
+
+            # multi-line text extraction
             "objectives": response.css('//*[@id="tabProfile"]/p[1]::text').get(),
             "vision_and_mission": response.css('//*[@id="tabProfile"]/p[2]::text').get(),
-            "organisation_activities": response.css('//*[@id="tabProfile"]/ul/li::text').getall(), # issue: data stored in seperate ul li elements
+            "organisation_activities": response.css('//*[@id="tabProfile"]/ul/li::text').getall(), # data stored in seperate ul li elements
+            
+            # extracting table data
+            "patrons": self.extract_table_data(response, '#tblPatrons'),
+            "governing_board_members": self.extract_table_data(response, '#tblGoverningBoardMembers'),
+            "key_officers": self.extract_table_data(response, '#tblKeyOfficers')
         }
 
+
         # issue: dealing with tables with pagination for patrons, board members & key officers
+        def extract_table_data(self, response, table_id):
+            table_data = [] # storing extract table data in this list
+            rows = response.css('//*[@id="table_id"]/tbody/tr')
+            # both key officers and governing board members table have a similar schema - extract Full Name & Designation
+            # patrons just need to extract their full names
+            for row in rows:
+                if row.css('td.dataTables_empty'):
+                    continue
+                else:
+
+            pass
+
+        # likely we need to check if pagination is present
+        # if pagination is present in the table, we would need to fetch the links - do note the href uses # which is likely JS
+        # extract table information - I would like to extract table by table to avoid clashing info
+        # skip table if no records found
